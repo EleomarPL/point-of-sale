bbd1abdb27ed67d2b4ca0bb5e94b3c7329695aae
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-09-10 10:35:59 -0500

    context with personalized routers

---
+++ src/components/router/AdminRouter.js
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+import Auth from '../../contexts/Auth';
+
+const AdminRouter = ({children}, props) => {
+  const {userData} = useContext(Auth);
+  let isLogged = userData === null ? false : true;
+
+  if (isLogged) {
+    if (userData.type === 1)
+      return <Redirect to="/my" />;
+    else if (userData.type === 0)
+      return <Route { ...props }>{ children }</Route>;
+  } else {
+    return <Redirect to="/" />;
+  }
+
+};
+AdminRouter.propTypes = {
+  children: PropTypes.node.isRequired
+};
+
+export default AdminRouter;
