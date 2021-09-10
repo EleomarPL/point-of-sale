b423b6dd769e504933472fad3507b65fa7d5dcf4
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-09-10 09:35:38 -0500

    dependencies to use with the possibility of adding more

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import '@fontsource/roboto';
+
 const App = () => {
   return (
     <p>Hola mundo</p>
