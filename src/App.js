774f520a8d483f23e953a34862cc15d36ac2f1f2
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-09-10 10:43:10 -0500

    base structure of the defined app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import {HashRouter, Switch} from 'react-router-dom';
 
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import '@fontsource/roboto';
 
+import {AuthProvider} from './contexts/Auth';
+import PublicRoute from './components/router/PublicRouter';
+import MyRouter from './components/router/MyRouter';
+import AdminRouter from './components/router/AdminRouter';
+import Home from './pages/Home';
+import My from './pages/My';
+import Admin from './pages/Admin';
+
 const App = () => {
   return (
-    <p>Hola mundo</p>
+    <AuthProvider>
+      <HashRouter>
+        <Switch>
+          <PublicRoute exact path="/">
+            <Home />
+          </PublicRoute>
+          <MyRouter exact path="/my">
+            <My />
+          </MyRouter>
+          <AdminRouter exact path="/admin">
+            <Admin />
+          </AdminRouter>
+        </Switch>
+      </HashRouter>
+    </AuthProvider>
   );
 };
 
---
+++ src/pages/Admin.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Admin = () => {
+  return (
+    <p>Admin</p>
+  );
+};
+
+export default Admin;
