638171cce9e0101fa34e7cec39ca64e98402bd96
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-09-10 09:23:49 -0500

    config rules of eslint and install dependencies to development mode

---
+++ public/electron.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron');
+ 
+function createWindow() {
+  const createWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      nativeWindowOpen: true
+    }
+  });
+  createWindow.loadURL('http://localhost:3000/');
+  // createWindow.loadFile(path.join(__dirname, '../', 'build' , 'index.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
