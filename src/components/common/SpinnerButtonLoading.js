229da35f59727522f547f9fbb0b7dc611f866b9a
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-10-02 13:25:41 -0500

    SpinnerButtonLoading added

---
+++ src/components/common/SpinnerButtonLoading.js
@@ -0,0 +1,10 @@
+const SpinnerButtonLoading = () => {
+  return (
+    <span className="spinner-border spinner-border-sm" role="status"
+      aria-hidden="true" style={ {marginRight: '0.5rem'} }
+    >
+    </span>
+  );
+};
+
+export default SpinnerButtonLoading;
