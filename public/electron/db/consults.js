f0d9cbca36e2d94ba8f350190a5168e02532b083
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-10-04 12:39:54 -0500

    functionality to login added

---
+++ public/electron/db/consults.js
@@ -0,0 +1,16 @@
+const {getConnection, closeConnection} = require('../connection');
+
+const login = async({username, password}) => {
+  const {connection, pool} = await getConnection();
+
+  const userInfo = await connection.query(
+    `SELECT * FROM user WHERE username='${username}' AND password='${password}';`
+  );
+  closeConnection({connection, pool});
+
+  return userInfo[0] || null;
+};
+
+module.exports = {
+  login
+};
