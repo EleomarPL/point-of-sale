8ad782b634e78f839c70e1675873b2441fb671eb
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-10-05 21:53:33 -0500

    functionality to get providers

--- public/electron/db/consults.js
+++ public/electron/db/consults.js
@@ -18,7 +18,17 @@ const isThereAnAdmin = async() => {
 
   return adminInfo[0] ? true : false;
 };
+const getProviders = async({value = '', limit}) => {
+  const {connection, pool} = await getConnection();
+  const getProviders = await connection.query(
+    `SELECT * FROM provider WHERE CONCAT(id, company, name, lastName) LIKE '%${value}%' ORDER BY Id 
+    ${limit ? 'DESC LIMIT 0,' + limit : ''};`
+  );
+  closeConnection({connection, pool});
+
+  return getProviders;
+};
 
 module.exports = {
-  login, isThereAnAdmin
+  login, isThereAnAdmin, getProviders
 };
