8f6ace7f6177cd0c5dd02ed69bcf103ddd26d32b
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-10-11 09:47:51 -0500

    events were added to the ipcRenderer

--- public/electron/preload.js
+++ public/electron/preload.js
@@ -1,12 +1,23 @@
 const {contextBridge, ipcRenderer} = require('electron');
 
+const validChannels = [];
+
 contextBridge.exposeInMainWorld(
   'electron', {
-    provider: {},
-    shopping: {},
-    article: {},
-    employee: {},
-    sales: {},
-    debts: {}
+    send: (channel, data) => {
+      if (validChannels.includes(channel)) {
+        ipcRenderer.send(channel, data);
+      }
+    },
+    on: (channel, callback) => {
+      if (validChannels.includes(channel)) {
+        ipcRenderer.on(channel, callback);
+      }
+    },
+    removeAllListeners: (channel) => {
+      if (validChannels.includes(channel)) {
+        ipcRenderer.removeAllListeners(channel);
+      }
+    }
   }
 );
