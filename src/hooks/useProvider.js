70ceac479c7ecc0efc8b2289ae58fb463656284b
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-10-18 18:38:09 -0500

    get providers after open modal functionality connected to react

--- src/hooks/useProvider.js
+++ src/hooks/useProvider.js
@@ -1,12 +1,14 @@
 const useProvider = () => {
   const createProvider = ({company, name, lastName, motherLastName}) => {
     window.electron.send('main:insert-provider', {name, lastName, motherLastName, company});
+    window.electron.send('main:get-provider', {keyword: '', limit: 50});
   };
   const getProviders = ({keyword, limit}) => {
     window.electron.send('main:get-provider', {keyword, limit});
   };
   const editProvider = ({id, name, lastName, motherLastName}) => {
     window.electron.send('main:update-provider', {id, name, lastName, motherLastName});
+    window.electron.send('main:get-provider', {keyword: '', limit: 50});
   };
   return {
     createProvider, getProviders, editProvider
