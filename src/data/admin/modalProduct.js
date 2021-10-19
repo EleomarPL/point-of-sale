fa99873eb08ff55e268f764043c84edc5968f706
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-10-19 12:37:29 -0500

    rename property

--- src/data/admin/modalProduct.js
+++ src/data/admin/modalProduct.js
@@ -2,5 +2,5 @@ export const inputProduct = [
   { type: 'text', placeholder: 'Producto', id: 'product' },
   { type: 'text', placeholder: 'Precio Compra', id: 'purchasePrice' },
   { type: 'text', placeholder: 'Precio Venta', id: 'salesPrice' },
-  { type: 'text', placeholder: 'Cantidad', id: 'stock' }
+  { type: 'text', placeholder: 'Cantidad', id: 'amount' }
 ];
