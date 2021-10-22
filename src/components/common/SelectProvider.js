3cf6d5d47f89a0dc7debe3685231d056c5604563
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-10-22 09:55:18 -0500

    get providers for select functionality connected to react

--- src/components/common/SelectProvider.js
+++ src/components/common/SelectProvider.js
@@ -1,11 +1,46 @@
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
+import useProvider from '../../hooks/useProvider';
+
 const SelectProvider = ({ widthSelect = '10%'}) => {
+  const [providers, setProviders] = useState([]);
+  const [providerSelect, setProviderSelect] = useState('');
+  const {getProviderForSelect} = useProvider();
+
+  useEffect(() => {
+    getProviderForSelect();
+    window.electron.on('render:get-provider-forselect', (err, data) => {
+      if (!err) {
+        console.log('error get article by id');
+        return null;
+      }
+      if (data[0] !== undefined) {
+        setProviders(data);
+        setProviderSelect(data[0].id);
+      }
+    });
+
+    return () => {
+      window.electron.removeAllListeners('render:get-provider-forselect');
+    };
+  }, []);
+
   return (
     <>
       <span className="mx-1">Proveedor: </span>
-      <select className="form-select mx-1" style={ {width: widthSelect} }>
-        <option value="default">Provider</option>
+      <select className="form-select mx-1" style={ {width: widthSelect} }
+        value={ providerSelect } onChange={ (evt) => setProviderSelect(evt.target.value) }
+      >
+        { providers &&
+          providers.map(provider =>
+            <option value={ provider.id }
+              key={ provider.id }
+            >
+              { provider.company }
+            </option>
+          )
+        }
       </select>
     </>
   );
--- src/hooks/useProvider.js
+++ src/hooks/useProvider.js
@@ -8,8 +8,11 @@ const useProvider = () => {
   const editProvider = ({id, name, lastName, motherLastName}) => {
     window.electron.send('main:update-provider', {id, name, lastName, motherLastName});
   };
+  const getProviderForSelect = () => {
+    window.electron.send('main:get-provider-forselect');
+  };
   return {
-    createProvider, getProviders, editProvider
+    createProvider, getProviders, editProvider, getProviderForSelect
   };
 };
 
