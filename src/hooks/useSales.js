2c29d4cd4875b2c8ed0075784d256527633d4fb8
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-10-27 09:56:57 -0500

    hook ti get stock sales was added

--- src/hooks/useSales.js
+++ src/hooks/useSales.js
@@ -2,9 +2,12 @@ const useSales = () => {
   const getStandardSales = ({value, startDate, endDate, limit}) => {
     window.electron.send('main:get-standard-sales', {value, startDate, endDate, limit});
   };
+  const getStockSales = ({value, startDate, endDate}) => {
+    window.electron.send('main:get-stock-sales', {value, startDate, endDate});
+  };
 
   return {
-    getStandardSales
+    getStandardSales, getStockSales
   };
 };
 
