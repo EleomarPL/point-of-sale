815447b86c9050b1346a7fe1403086f49f10c94c
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-11-05 09:14:23 -0600

    functionality to managment change and result change

--- src/components/views/my/BoxStatusSales.js
+++ src/components/views/my/BoxStatusSales.js
@@ -1,12 +1,28 @@
 import PropTypes from 'prop-types';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import ButtonPersonalized from '../../common/ButtonPersonalized';
 import { openmodalDebts } from '../../modals/ModalDebts';
 import SalesContext from '../../../contexts/Sales';
+import { isNumberValue } from '../../../services/validations/generalValidations';
 
 const BoxStatusSales = ({dataSelected, setDataSelected}) => {
+  const [totalSales, setTotalSales] = useState(0);
+  const [payment, setPayment] = useState(0);
+
   const {listSales} = useContext(SalesContext);
+
+  useEffect(() => {
+    setTotalSales(listSales.reduce((acc, current) => acc + current.total, 0));
+    setPayment(0);
+  }, [listSales]);
+
+  const handleValidatePayment = (evt) => {
+    if (isNumberValue({name: 'Pago', value: evt.target.value}))
+      setPayment(Number(evt.target.value));
+    else
+      setPayment(0);
+  };
 
   return (
     <>
@@ -17,15 +33,14 @@ const BoxStatusSales = ({dataSelected, setDataSelected}) => {
         </span>
         <span className="fw-bold">Total:</span>
         <span className="fw-bold">
-          { listSales &&
-            Number(listSales.reduce((acc, current) => acc + current.total, 0)).toFixed(2)
-          }
+          { listSales && totalSales.toFixed(2) }
         </span>
       </div>
       <div className="d-flex flex-column align-items-center">
         <button
           type="button"
           className="button-personalized is-menu text-black text-decoration-none w-75"
+          disabled={ totalSales ? payment < totalSales : true }
         >
           <ButtonPersonalized isColumn={ true } classNameIcon="bi bi-archive-fill">
             Cobrar
@@ -45,11 +60,18 @@ const BoxStatusSales = ({dataSelected, setDataSelected}) => {
         <span id="paying" style={ {marginRight: '10px'} }>Pago: </span>
         <input type="text" className="form-control"
           aria-label="Paying" aria-describedby="paying"
+          value={ payment } onChange={ (evt) => handleValidatePayment(evt) }
+          disabled={ totalSales === 0 }
         />
       </div>
       <div className="mb-3">
         <span id="paying" style={ {marginRight: '10px'} }>Cambio: </span>
-        <span style={ {fontSize: '1.5rem'} }>0000.00</span>
+        <span style={ {fontSize: '1.5rem'} }>
+          { totalSales < payment
+            ? Number(payment - totalSales).toFixed(2)
+            : '0.00'
+          }
+        </span>
       </div>
     </>
   );
