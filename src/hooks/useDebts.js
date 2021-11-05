43c3974a8d776f0234e2a53557d928abaec817e3
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-11-05 10:00:26 -0600

    hook to insert debtor was added

--- src/hooks/useDebts.js
+++ src/hooks/useDebts.js
@@ -2,9 +2,12 @@ const useDebts = () => {
   const getDebtsByKeyword = ({value}) => {
     window.electron.send('main:get-debts', {value, isGroupByDebtor: true});
   };
+  const insertDebtor = ({name, lastName, motherLastName, isAMan, address}) => {
+    window.electron.send('main:insert-debtor', {name, lastName, motherLastName, isAMan, address});
+  };
 
   return {
-    getDebtsByKeyword
+    getDebtsByKeyword, insertDebtor
   };
 };
 
