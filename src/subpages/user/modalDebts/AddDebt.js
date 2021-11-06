7960fa06a209de75f7a2690f41dbb389071cf5ad
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-11-06 09:18:17 -0600

    functionality to show total on future debt

--- src/subpages/user/modalDebts/AddDebt.js
+++ src/subpages/user/modalDebts/AddDebt.js
@@ -1,12 +1,19 @@
-import { useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import ButtonPersonalized from '../../../components/common/ButtonPersonalized';
 import SelectDebtor from '../../../components/common/SelectDebtor';
 import SpinnerButtonLoading from '../../../components/common/SpinnerButtonLoading';
+import SalesContext from '../../../contexts/Sales';
 
 const AddDebt = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [debtorSelect, setDebtorSelect] = useState('none');
+  const [total, setTotal] = useState(0);
+  const {listSales} = useContext(SalesContext);
+
+  useEffect(() => {
+    setTotal(listSales.reduce((acc, current) => current.total + acc, 0));
+  }, [listSales]);
 
   const handleAddDebt = () => {
     setIsLoading(true);
@@ -15,7 +22,7 @@ const AddDebt = () => {
   return (
     <div className="w-100">
       <div className="w-100 text-center my-2" style={ {fontSize: '1.8rem'} }>
-        <span>Total Deuda: <strong>000.00</strong></span>
+        <span>Total Deuda: <strong>{ total.toFixed(2) }</strong></span>
       </div>
       <div className="d-flex justify-content-center align-items-center my-2">
         <label htmlFor="select-debt" className="px-2">Seleccionar Deudor:</label>
