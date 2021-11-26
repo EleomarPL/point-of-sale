ddad8a18d7d9ba685c80c958fb6df466a41c1a8b
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-11-26 09:48:31 -0600

    added visually hidden in SpinnerButtonLoading component

--- src/components/common/SpinnerButtonLoading.js
+++ src/components/common/SpinnerButtonLoading.js
@@ -3,6 +3,7 @@ const SpinnerButtonLoading = () => {
     <span className="spinner-border spinner-border-sm" role="status"
       aria-hidden="true" style={ {marginRight: '0.5rem'} }
     >
+      <span className="visually-hidden">Loading...</span>
     </span>
   );
 };
