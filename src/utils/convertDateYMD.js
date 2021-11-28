cc216df56a579d7de81ea28b00af22440b712139
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-11-27 19:16:27 -0600

    added comment in convertDateYMD util

--- src/utils/convertDateYMD.js
+++ src/utils/convertDateYMD.js
@@ -1,3 +1,5 @@
+// utility to add zero to date
+
 export const convertDateYYYYMMDD = ({day, month, year}) => {
   let monthWitZero = ('0' + month).slice(-2);
   let dayWithZero = ('0' + day).slice(-2);
