f4f2f33be2bca4448197d245bec677c4d762f559
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2022-03-02 11:23:28 -0600

    create sql structure was added in main proccess

--- public/electron/common/channelsInitialOperations.js
+++ public/electron/common/channelsInitialOperations.js
@@ -1,3 +1,4 @@
 module.exports = [
-  'render:validate-connection-to-db', 'main:validate-connection-to-db'
+  'render:validate-connection-to-db', 'main:validate-connection-to-db',
+  'render:create-sql-structure', 'main:create-sql-structure'
 ];
--- public/electron/evtContextBridge/initialOperations.js
+++ public/electron/evtContextBridge/initialOperations.js
@@ -1,12 +1,19 @@
 const { ipcMain } = require('electron');
 
-const { validateConnectionToDB } = require('../db/initialOperations');
+const {
+  validateConnectionToDB, createSQLStructure
+} = require('../db/initialOperations');
 
 const triggerEventsInitialOperations = ({windowToSend}) => {
   ipcMain.on('main:validate-connection-to-db', async() => {
     const isSuccessfulConnection = await validateConnectionToDB();
     
     windowToSend.webContents.send('render:validate-connection-to-db', isSuccessfulConnection);
+  });
+  ipcMain.on('main:create-sql-structure', async() => {
+    await createSQLStructure();
+    
+    windowToSend.webContents.send('render:create-sql-structure', {result: true});
   });
 };
 
