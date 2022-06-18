39749a2d3c4c383ba37749dca03b691eb432caf6
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2022-06-18 07:40:34 -0500

    feat: View for test connection with mariadb was added

--- src/pages/Connection.js
+++ src/pages/Connection.js
@@ -1,6 +1,87 @@
+import { useEffect, useRef, useState } from 'react';
+
+import SpinnerButtonLoading from '../components/common/SpinnerButtonLoading';
+
 const Connection = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const inputHostRef = useRef({});
+  const inputPortRef = useRef({});
+  const inputUserRef = useRef({});
+  const inputDatabsaseRef = useRef({});
+
+  useEffect(() => {
+    inputHostRef.current.value = 'localhost';
+    inputUserRef.current.focus();
+
+    inputPortRef.current.value = '3306';
+    inputDatabsaseRef.current.value = 'point_of_sale';
+  }, []);
+
+  const handleVerifyConnection = (evt) => {
+    evt.preventDefault();
+
+    setIsLoading(true);
+  };
+
   return (
-    <p>Conexion</p>
+    <section className="col-md-12 min-vh-100" style={ {backgroundColor: '#BED7AA'} }>
+      <h1 className="text-center">Conexión a la base de datos</h1>
+      <form className="w-50 m-auto mt-4" onSubmit={ handleVerifyConnection }>
+        <div className="input-group row row-cols-lg-4 g-1 mb-2">
+          <label className="col-form-label" htmlFor="host">Host</label>
+          <input
+            type="text" id="host"
+            className="form-control flex-fill"
+            style={ {backgroundColor: '#f6eded'} }
+            ref={ inputHostRef }
+          />
+        </div>
+        <div className="input-group row row-cols-lg-4 g-1 mb-2">
+          <label className="col-form-label" htmlFor="port">Puerto</label>
+          <input
+            type="text" id="port"
+            className="form-control flex-fill"
+            style={ {backgroundColor: '#f6eded'} }
+            ref={ inputPortRef }
+          />
+        </div>
+        <div className="input-group row row-cols-lg-4 g-1 mb-2">
+          <label className="col-form-label" htmlFor="user">Usuario</label>
+          <input
+            type="text" id="user"
+            className="form-control flex-fill"
+            style={ {backgroundColor: '#f6eded'} }
+            ref={ inputUserRef }
+          />
+        </div>
+        <div className="input-group row row-cols-lg-4 g-1 mb-2">
+          <label className="col-form-label" htmlFor="password">Contraseña</label>
+          <input
+            type="text" id="password"
+            className="form-control flex-fill"
+            style={ {backgroundColor: '#f6eded'} }
+          />
+        </div>
+        <div className="input-group row row-cols-lg-4 g-1 mb-2">
+          <label className="col-form-label" htmlFor="database">Base de datos</label>
+          <input
+            type="text" id="database"
+            className="form-control flex-fill"
+            style={ {backgroundColor: '#f6eded'} }
+            ref={ inputDatabsaseRef }
+          />
+        </div>
+        <div className="text-center">
+          <button type="submit" className="btn btn-primary"
+            disabled={ isLoading }
+          >
+            { isLoading && <SpinnerButtonLoading /> }
+            Conectar
+          </button>
+        </div>
+      </form>
+    </section>
   );
 };
 
